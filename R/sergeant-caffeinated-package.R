c4a54350cfb9aacb1bdf7c880ff63bb7fa651c44
boB Rudis <bob@rud.is>
2018-10-14 08:28:41 -0400

    R package repo initialization complete

---
+++ R/sergeant-caffeinated-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/sergeant-caffeinated>
+#' - BugReports: <https://gitlab.com/hrbrmstr/sergeant-caffeinated/issues>
+#' 
+#' @md
+#' @name sergeant-caffeinated
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
